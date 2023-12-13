ea99870d18027e855d137813ca3763070fe1e112
Stefan Gouyet <stefangouyet@Stefans-MacBook-Pro.local>
2023-12-13 06:32:51 -0500

    Add config.js

---
+++ src/config.js
@@ -0,0 +1,384 @@
+var config = {
+  style: "mapbox://styles/geocarte/ckiujx22y0n0d19lcmm6tbv34",
+  accessToken:
+    "pk.eyJ1Ijoic3RlZmFuZ291eWV0IiwiYSI6ImNscHF0dHhhNjAzeGoyam8wcTFqZjlnbW4ifQ.NbIIrM1ACugwdFe6_V2d3Q",
+  showMarkers: true,
+  markerColor: "#0766FF",
+  inset: true,
+  title: "Visualizing 30 years of Champions League Finals",
+  chapters: [
+    {
+      id: "slug-style-id",
+      hidden: false,
+      title: "1993 Final",
+      subTitle: "Marseille 1 - 0 Milan",
+      image: "https://upload.wikimedia.org/wikipedia/commons/2/2a/1993_UEFA_Champion%27s_League_final_-_Olympique_de_Marseille_vs_AC_Milan_Line-up.png",
+      description:
+        "Goal by Basile Boli",
+      stadium: "Olympiastadion, Munich",
+      location: {
+        center: [11.546631, 48.1731805],
+      },
+      callback: "",
+    },
+    {
+      id: "second-identifier",
+      hidden: false,
+      title: "1994 Final",
+      subTitle: "Barcelona 0 - 4 Milan",
+      description:
+        "Goals by Daniele Massaro (2), Dejan Savicevic, and Marcel Desailly",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Milan-Barcelona_1994-05-18.svg/300px-Milan-Barcelona_1994-05-18.svg.png",
+      stadium: "Olympic Stadium, Athens",
+      location: {
+        center: [23.7873795, 38.0362115],
+      },
+    },
+    {
+      id: "third-identifier",
+      title: "1995 Final",
+      subTitle: "Ajax 1 - 0 Milan",
+      description:
+        "Goal by Patrick Kluivert",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Ajax-Milan_1995-05-24.svg/600px-Ajax-Milan_1995-05-24.svg.png",
+      stadium: "Ernst-Happel-Stadion, Vienna",
+      location: {
+        center: [16.420485499999998, 48.207363],
+      },
+    },
+    {
+      id: "fourth-identifier",
+      title: "1996 Final",
+      subTitle: "Ajax 1 - 1 Juventus (Juventus won 4-2 on Penalties)",
+      description:
+        "Goals by Fabrizio Ravanelli and Jari Litmanen",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Ajax_vs_Juventus_1996-05-22.svg/600px-Ajax_vs_Juventus_1996-05-22.svg.png",
+      stadium: "Stadio Olimpico, Rome",
+      location: {
+        center: [12.4547595, 41.933893499999996],
+      },
+    },
+    {
+      id: "5th-identifier",
+      title: "1997 Final",
+      subTitle: "Borussia Dortmund 3 - 1 Juventus",
+      description:
+        "Goals by Karl-Heinz Riedle (2), Lars Ricken, and Alessandro Del Piero",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Borussia_Dortmund_vs_Juventus_1997-05-28.svg/600px-Borussia_Dortmund_vs_Juventus_1997-05-28.svg.png",
+      stadium: "Olympiastadion, Munich",
+      location: {
+        center: [11.546631, 48.1731805],
+      },
+    },
+    {
+      id: "6th-identifier",
+      title: "1998 Final",
+      subTitle: "Real Madrid 1 - 0 Juventus",
+      description:
+        "Goal by Predrag Mijatović",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Real_Madrid_vs_Juventus_1998-05-20.svg/600px-Real_Madrid_vs_Juventus_1998-05-20.svg.png",
+      stadium: "Amsterdam Arena, Amsterdam",
+      location: {
+        center: [4.941362, 52.314688],
+      },
+    },
+    {
+      id: "7th-identifier",
+      title: "1999 Final",
+      subTitle: "Manchester United 2 - 1 Bayern Munich",
+      description:
+        "Goals by Mario Basler, Edward Sheringham, and Ole Gunnar Solksjaer ",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Man_Utd_vs_Bayern_Munich_1999-05-26.svg/600px-Man_Utd_vs_Bayern_Munich_1999-05-26.svg.png",
+      stadium: "Camp Nou, Barcelona",
+      location: {
+        center: [2.1226215, 41.3808135],
+      },
+    },
+    {
+      id: "8th-identifier",
+      title: "2000 Final",
+      subTitle: "Real Madrid 3 - 0 Valencia",
+      description:
+        "Goals by Fernando Morientes, Steven McManaman, Raul",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Real_Madrid_vs_Valencia_2000-05-24.svg/600px-Real_Madrid_vs_Valencia_2000-05-24.svg.png",
+      stadium: "Stade de France, Saint-Denis",
+      location: {
+        center: [2.360122, 48.9244555],
+      },
+    },
+    {
+      id: "9th-identifier",
+      title: "2001 Final",
+      subTitle: "Bayern Munich 1 - 1 Valencia (Bayern Munich won 5-4 on penalties)",
+      description:
+        "Goals by Gaizka Mendieta and Stefan Effenberg",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Bayern_Munich_vs_Valencia_2001-05-23.svg/600px-Bayern_Munich_vs_Valencia_2001-05-23.svg.png",
+      stadium: "San Siro, Milan",
+      location: {
+        center: [9.125243, 45.474875],
+      },
+    },
+    {
+      id: "10th-identifier",
+      title: "2002 Final",
+      subTitle: "Bayer Leverkusen 1 - 2 Real Madrid",
+      description:
+        "Goals by Raul, Lucio, and Zinedine Zidane",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Bayer_Leverkusen_vs_Real_Madrid_2002-05-15.svg/600px-Bayer_Leverkusen_vs_Real_Madrid_2002-05-15.svg.png",
+      stadium: "Hampden Park, Glasgow",
+      location: {
+        center: [-4.251912, 55.825713500000006],
+      },
+    },
+    {
+      id: "11th-identifier",
+      title: "2003 Final",
+      subTitle: "Juventus 0 - 0 Milan (Milan won 3-2 on penalties)",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Juventus_vs_Milan_2003-05-28.svg/600px-Juventus_vs_Milan_2003-05-28.svg.png",
+      stadium: "Old Trafford, Trafford",
+      location: {
+        center: [-2.29035275, 53.46317725],
+      },
+    },
+    {
+      id: "11th2-identifier",
+      title: "2004 Final",
+      subTitle: "Monaco 0 - 3 Porto",
+      description:
+        "Goals by Carlos Alberto, Deco, and Dmitri Alenichev",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/AS_Monaco_vs_Porto_2004-05-26.svg/600px-AS_Monaco_vs_Porto_2004-05-26.svg.png",
+      stadium: "Arena AufSchalke, Gelsenkirchen",
+      location: {
+        center: [7.067594, 51.5545955],
+      },
+    },
+    {
+      id: "12th-identifier",
+      title: "2005 Final",
+      subTitle: "Milan 3 - 3 Liverpool (Liverpool won 3-2 on penalties)",
+      description:
+        "Goals by Paolo Maldini, Hernan Crespo (2), Steven Gerrard, Vladimir Smicer, and Xabi Alonso",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Milan_vs_Liverpool_2005-05-25.svg/600px-Milan_vs_Liverpool_2005-05-25.svg.png",
+      stadium: "Atatürk Olympic Stadium, Istanbul",
+      location: {
+        center: [28.7657575, 41.0746605],
+      },
+    },
+    {
+      id: "13th-identifier",
+      title: "2006 Final",
+      subTitle: "Barcelona 2 - 1 Arsenal",
+      description:
+        "Goals by Sol Campbell, Samuel Eto'o, and Juliano Belletti",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Barcelona_vs_Arsenal_2006-05-17.svg/600px-Barcelona_vs_Arsenal_2006-05-17.svg.png",
+      stadium: "Stade de France, Saint-Denis",
+      location: {
+        center: [2.360122, 48.9244555],
+      },
+    },
+    {
+      id: "14th-identifier",
+      title: "2007 Final",
+      subTitle: "Milan 2 - 1 Liverpool",
+      description:
+        "Goals by Filippo Inzaghi (2) and Dirk Kuyt",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Milan_vs_Liverpool_2007-05-23.svg/600px-Milan_vs_Liverpool_2007-05-23.svg.png",
+      stadium: "Olympic Stadium, Athens",
+      location: {
+        center: [23.7873795, 38.0362115],
+      },
+    },
+    {
+      id: "15th-identifier",
+      title: "2008 Final",
+      subTitle: "Manchester United 1 - 1 Chelsea (Manchester United won 6-5 on penalties)",
+      description:
+        "Goals by Cristiano Ronaldo and Frank Lampard",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Man_Utd_vs_Chelsea_2008-05-21.svg/600px-Man_Utd_vs_Chelsea_2008-05-21.svg.png",
+      stadium: "Luzhniki Stadium, Moscow",
+      location: {
+        center: [37.553684625, 55.715790625],
+      },
+    },
+    {
+      id: "16th-identifier",
+      title: "2009 Final",
+      subTitle: "Barcelona 2 - 0 Manchester United",
+      description:
+        "Goals by Samuel Eto'o and Lionel Messi",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Barcelona_vs_Man_Utd_2009-05-27.svg/600px-Barcelona_vs_Man_Utd_2009-05-27.svg.png",
+      stadium: "Stadio Olimpico, Rome",
+      location: {
+        center: [12.4547595, 41.933893499999996],
+      },
+    },
+    {
+      id: "17th-identifier",
+      title: "2010 Final",
+      subTitle: "Bayern Munich 0 - 2 Inter Milan",
+      description:
+        "Goals by Diego Milito (2)",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Bayern_Munich-Internazionale_2010-05-22.svg/600px-Bayern_Munich-Internazionale_2010-05-22.svg.png",
+      stadium: "Santiago Bernabéu, Madrid",
+      location: {
+        center: [-3.687584, 40.452757],
+      },
+    },
+    {
+      id: "18th-identifier",
+      title: "2011 Final",
+      subTitle: "Barcelona 3 - 1 Manchester United",
+      description:
+        "Goals by Pedro, Wayne Rooney, Lionel Messi, and David Villa",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Barcelona_vs_Man_Utd_2011-05-28.svg/600px-Barcelona_vs_Man_Utd_2011-05-28.svg.png",
+      stadium: "Wembley Stadium, London",
+      location: {
+        center: [-0.279508, 51.5558825],
+      },
+    },
+    {
+      id: "19th-identifier",
+      title: "2012 Final",
+      subTitle: "Bayern Munich 1 - 1 Chelsea (Chelsea won 4-3 on penalties)",
+      description:
+        "Goals by Thomas Muller and Didier Drogba",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Bayern_Munich_vs_Chelsea_2012-05-19.svg/600px-Bayern_Munich_vs_Chelsea_2012-05-19.svg.png",
+      stadium: "Allianz Arena, Munich",
+      location: {
+        center: [11.6247075, 48.218832500000005],
+      },
+    },
+    {
+      id: "20th-identifier",
+      title: "2013 Final",
+      subTitle: "Borussia Dortmund 1 - 2 Bayern Munich",
+      description:
+        "Goals by Mario Mandzukic, Ilkay Gundogan, and Arjen Robben",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Borussia_Dortmund_vs_Bayern_Munich_2013-05-25.svg/600px-Borussia_Dortmund_vs_Bayern_Munich_2013-05-25.svg.png",
+      stadium: "Wembley Stadium, London",
+      location: {
+        center: [-0.279508, 51.5558825],
+      },
+    },
+    {
+      id: "21th-identifier",
+      title: "2014 Final",
+      subTitle: "Real Madrid 4 - 1 Atletico Madrid",
+      description:
+        "Goals by Diego Godin, Sergio Ramos, Gareth Bale, Marcelo, and Cristiano Ronaldo",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Real_Madrid_vs_Atl%C3%A9tico_Madrid_2014-05-24.svg/600px-Real_Madrid_vs_Atl%C3%A9tico_Madrid_2014-05-24.svg.png",
+      stadium: "Estádio da Luz, Lisbon",
+      location: {
+        center: [-9.183658000000001, 38.75312275],
+      },
+    },
+    {
+      id: "22th-identifier",
+      title: "2015 Final",
+      subTitle: "Juventus 1 - 3 Barcelona",
+      description:
+        "Goals by Ivan Rakitic, Alvaro Morata, Luis Suarez, and Neymar",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Juventus_vs_Barcelona_2015-06-06.svg/600px-Juventus_vs_Barcelona_2015-06-06.svg.png",
+      stadium: "Olympiastadion, Berlin",
+      location: {
+        center: [13.239103499999999, 52.515743],
+      },
+    },
+    {
+      id: "23th-identifier",
+      title: "2016 Final",
+      subTitle: "Real Madrid 1 - 1 Atletico Madrid (Real Madrid won 5 - 3 on penalties",
+      description:
+        "Goals by Sergio Ramos and Yannick Carrasco",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Real_Madrid_vs_Atl%C3%A9tico_Madrid_2016-05-28.svg/600px-Real_Madrid_vs_Atl%C3%A9tico_Madrid_2016-05-28.svg.png",
+      stadium: "San Siro, Milan",
+      location: {
+        center: [9.125243, 45.474875],
+      },
+    },
+    {
+      id: "24th-identifier",
+      title: "2017 Final",
+      subTitle: "Juventus 1 - 4 Real Madrid",
+      description:
+        "Goals by Cristiano Ronaldo (2), Mario Mandzukic, Casemiro, and Marco Asensio",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Real_Madrid_vs_Atl%C3%A9tico_Madrid_2016-05-28.svg/600px-Real_Madrid_vs_Atl%C3%A9tico_Madrid_2016-05-28.svg.png",
+      stadium: "Millennium Stadium, Cardiff",
+      location: {
+        center: [-3.1824455, 51.47833],
+      },
+    },
+    {
+      id: "25th-identifier",
+      title: "2018 Final",
+      subTitle: "Real Madrid 3 - 1 Liverpool",
+      description:
+        "Goals by Karim Benzema, Sadio Mane, and Gareth Bale (2)",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Real_Madrid_vs_Liverpool_2018-05-26.svg/600px-Real_Madrid_vs_Liverpool_2018-05-26.svg.png",
+      stadium: "NSC Olimpiyskiy Stadium, Kyiv",
+      location: {
+        center: [30.521822, 50.433441],
+      },
+    },
+    {
+      id: "26th-identifier",
+      title: "2019 Final",
+      subTitle: "Tottenham Hotspur 0 - 2 Liverpool",
+      description:
+        "Goals by Mo Salah and Divock Origi",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Tottenham_Hotspur_vs_Liverpool_2019-06-01.svg/600px-Tottenham_Hotspur_vs_Liverpool_2019-06-01.svg.png",
+      stadium: "Metropolitano Stadium, Madrid",
+      location: {
+        center: [-3.599820875, 40.43547087499999],
+      },
+    },
+    {
+      id: "27th-identifier",
+      title: "2020 Final",
+      subTitle: "Paris Saint-Germain 0 - 1 Bayern Munich",
+      description:
+        "Goal by Kingsley Coman",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Paris_Saint-Germain_vs_Bayern_Munich_2020-08-23.svg/600px-Paris_Saint-Germain_vs_Bayern_Munich_2020-08-23.svg.png",
+      stadium: "Estádio da Luz, Lisbon",
+      location: {
+        center: [-9.183658000000001, 38.75312275],
+      },
+    },
+    {
+      id: "28th-identifier",
+      title: "2021 Final",
+      subTitle: "Manchester City 0 - 1 Chelsea",
+      description:
+        "Goal by Kai Havertz",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Manchester_City_vs_Chelsea_2021-05-29.svg/600px-Manchester_City_vs_Chelsea_2021-05-29.svg.png",
+      stadium: "Estádio do Dragão, Porto",
+      location: {
+        center: [-8.5838645, 41.1617805],
+      },
+    },
+    {
+      id: "29th-identifier",
+      title: "2022 Final",
+      subTitle: "Liverpool 0 - 1 Real Madrid",
+      description:
+        "Goal by Vinicius",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Liverpool_vs_Real_Madrid_2022-05-28.svg/600px-Liverpool_vs_Real_Madrid_2022-05-28.svg.png",
+      stadium: "Estádio do Dragão, Porto",
+      location: {
+        center: [-8.5838645, 41.1617805],
+      },
+    },
+    {
+      id: "30th-identifier",
+      title: "2023 Final",
+      subTitle: "Manchester City 1 - 0 Inter Milan",
+      description:
+        "Goal by Rodri",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Manchester_City_vs_Internazionale_2023-06-10.svg/600px-Manchester_City_vs_Internazionale_2023-06-10.svg.png",
+      stadium: "Atatürk Olympic Stadium, Istanbul",
+      location: {
+        center: [28.7657575, 41.0746605],
+      },
+    },
+  ],
+};
